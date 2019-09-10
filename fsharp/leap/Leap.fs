3016c9d1c93267b2f87068d666a409a5e082583b
csgrdcelia <casagrande.celia@gmail.com>
2019-09-10 15:29:00 +0200

    Complete: leap (fsharp)

---
+++ fsharp/leap/Leap.fs
@@ -0,0 +1,8 @@
+﻿module Leap
+
+let leapYear (year: int): bool =
+    match year with
+    | year when year % 4 <> 0 -> false
+    | year when year % 100 <> 0 -> true
+    | year when year % 400 <> 0 -> false
+    | _ -> true
