db40f8aaa49a58ac86ac7b4e3a7de50c3b61813c
csgrdcelia <casagrande.celia@gmail.com>
2019-09-11 10:36:39 +0200

    Implem: accumulate (fsharp) -> not tail recursive

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,8 @@
+﻿module Accumulate
+
+// Pas une récursion terminale
+let rec accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
+    match input with
+    | [] -> []
+    | head :: tail ->
+        func head :: (accumulate func tail)
