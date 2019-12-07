d99e8d68279c85a235624a225bc59583ef270c4f
csgrdcelia <casagrande.celia@gmail.com>
2019-12-07 16:41:16 +0100

    restructure

---
+++ fsharp/exercises/queen-attack/QueenAttack.fs
@@ -0,0 +1,9 @@
+﻿module QueenAttack
+
+let between (number1: int) (number2: int) (input: int) =
+    input > number1 && input < number2
+
+let create (position: int * int) =
+    (fst position |> between 0 8) && (snd position |> between 0 8) 
+
+let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
