8ee0cd0d767f214caf9fbdf98fee843e2f833d71
csgrdcelia <casagrande.celia@gmail.com>
2019-12-08 23:15:56 +0100

    WIP: hamming (ruby)

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,10 @@
+class Hamming
+  def self.compute(first_string, second_string)
+    raise ArgumentError if first_string.length != second_string.length
+    result = 0
+    for position in 0...first_string.length
+      result += 1 if first_string[position] == second_string[position]
+    end 
+    result
+  end
+end
