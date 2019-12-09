a38d6a1894f9ffefc86fc634b6516d4b1919d3e0
csgrdcelia <casagrande.celia@gmail.com>
2019-12-09 22:55:47 +0100

    Complete: hamming (update) (ruby)

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,6 @@
 class Hamming
   def self.compute(first_string, second_string)
     raise ArgumentError if first_string.length != second_string.length
-    result = 0
-    for position in 0...first_string.length
-      result += 1 if first_string[position] != second_string[position]
-    end 
-    result
+    0.upto(first_string.length).count { |i| first_string[i] != second_string[i] }
   end
 end
